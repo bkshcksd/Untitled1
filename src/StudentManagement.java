bbcaeec20c5bab669ab573c5a37b2f4029eb2fae
bkshcksd <suvamshakya43@gmail.com>
2026-09-04 16:22:01 +0545

    update

--- src/StudentManagement.java
+++ src/StudentManagement.java
@@ -2,15 +2,22 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class StudentManagement {
-    private static final String FILE ="students.txt";
+    private static final String FILE = "students.txt";
 
-    public void addStudents(String[] data){
-        try(FileWriter fw=new FileWriter(FILE,true)){
-            fw.write(String.join(",",data)+"\n");
+    public void addStudents(String[] data) {
+        try (FileWriter fw = new FileWriter(FILE, true)) {
+            fw.write(String.join(",", data) + "\n");
         } catch (IOException e) {
             System.out.println("error saving student");
         }
     }
 
+    public double calculateAverage(String[] data) {
+        int math = Integer.parseInt(data[2]);
+        int science = Integer.parseInt(data[3]);
+        int english = Integer.parseInt(data[4]);
+
+        return  (math = science = english)/3.0;
+    }
 
 }
